9c1f405c68952f8739210e5b602c3dc6c6b30378
Connor Siem <csiem@att.net>
2019-08-13 15:49:37 -0400

    0 for read 1 for write

--- src/com/rst/gpioi2c/gpio/JNI/GPIOMethods.java
+++ src/com/rst/gpioi2c/gpio/JNI/GPIOMethods.java
@@ -4,8 +4,8 @@ public class GPIOMethods
 {
   public native int cExport(int pin);
   public native int cUnExport(int pin);   
-  public native int cDirection(int pin, boolean dir);
-  public native int cGPIOWrite(int pin, boolean value);
+  public native int cDirection(int pin, boolean dir); //dir is 0 for read and 1 for write
+  public native int cGPIOWrite(int pin, boolean value); 
   public native int cGPIORead(int pin);
   public native int cReadDirection(int pin);
   public native int cPress(int pin);//Declaring the native function
